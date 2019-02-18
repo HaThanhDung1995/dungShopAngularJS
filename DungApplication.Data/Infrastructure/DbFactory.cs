b12bb227f6d82545171fb078a671f27f77fd988b
HaThanhDung29081995 <40621304+HaThanhDung29081995@users.noreply.github.com>
2019-02-18 11:49:48 +0700

    ThirdCommit

---
+++ DungApplication.Data/Infrastructure/DbFactory.cs
@@ -0,0 +1,20 @@
+﻿using DungApplication.Model.Models;
+
+namespace DungApplication.Data.Infrastructure
+{
+    public class DbFactory : Disposable, IDbFactory
+    {
+        private DataContext dbContext;
+
+        public DataContext Init()
+        {
+            return dbContext ?? (dbContext = new DataContext());
+        }
+
+        protected virtual void DisposeCore()
+        {
+            if (dbContext != null)
+                dbContext.Dispose();
+        }
+    }
+}
---
+++ DungApplication.Data/Infrastructure/IDbFactory.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DungApplication.Model.Models;
+
+namespace DungApplication.Data.Infrastructure
+{
+    public interface IDbFactory:IDisposable
+    {
+        DataContext Init();
+    }
+}
